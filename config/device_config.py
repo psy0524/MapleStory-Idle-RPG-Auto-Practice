d5d7bac1987af9793e541ba33424681810ca4831
psy0524 <parksangy@naver.com>
2026-05-06 18:20:18 +0900

    패키지명과 액티비티 명 환경변수 파일에서 수정하도록 수정

--- config/device_config.py
+++ config/device_config.py
@@ -1,14 +1,14 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # .env 파일 자동으로 읽어옴
+load_dotenv()  # .env 파일을 읽어들임
 
 DEVICE_CONFIG = {
     "platformName": "Android",
-    "platformVersion": os.getenv("PLATFORM_VERSION", "16"),
-    "deviceName": os.getenv("DEVICE_NAME", "YOUR_DEVICE_ID"),
-    "appPackage": "com.nexon.ma",
-    "appActivity": "com.nexon.ma.MainActivity",
+    "platformVersion": os.getenv("PLATFORM_VERSION", "16"), # .env에서 가져옴
+    "deviceName": os.getenv("DEVICE_NAME"),       # .env에서 가져옴
+    "appPackage": os.getenv("APP_PACKAGE"),       # .env에서 가져옴
+    "appActivity": os.getenv("APP_ACTIVITY"),     # .env에서 가져옴
     "noReset": True,
     "autoGrantPermissions": True,
 }
